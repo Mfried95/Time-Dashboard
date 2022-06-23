86ed030ca10fd5f58b55d4736232544ad72322f2
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-06-23 16:06:25 -0400

    Started Js

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
-fetch("./data.json")
+
+
+// Daily Button
+
+document.getElementById("daily-btn").addEventListener("click", function(){
+  fetch("./data.json")
   .then(response => response.json())
   .then(data => {
-    console.log(data[1].timeframes.weekly.current)
+  document.getElementById("work-hours").innerText = data[0].timeframes.daily
+  console.log()
   });
+})
+
+
+
+
+// Weekly Button
+
+document.getElementById("weekly-btn").addEventListener("click", daily)
+
+function daily(){
+  fetch("./data.json")
+  .then(response => response.json())
+  .then(data => {
+  
+  });
+
+}
+
+
+
+// Monthly Button
+
+document.getElementById("monthly-btn").addEventListener("click", daily)
+
+function daily(){
+  fetch("./data.json")
+  .then(response => response.json())
+  .then(data => {
+  
+  });
+
+}
