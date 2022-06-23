672efb4870d302d63ce58f0b7cbe17fda874d0c0
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-06-23 16:34:47 -0400

    Updated JS

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
 
 
+// All Hours and Previous hour variables
+
+const workHours  = document.getElementById("work-hours")
+const workPrevious = document.getElementById("previous-work-hours")
+
+const playhours = document.getElementById("play-hours")
+const playPrevious = document.getElementById("play-previous")
+
+const studyHours = document.getElementById("study-hours")
+const studyPrevious = document.getElementById("study-previous") 
+
+const excerciseHours = document.getElementById("excercise-hours")
+const excercisePrevious = document.getElementById("excercise-previous")
+
+const socialHours = document.getElementById("social-hours")
+const socialPrevious = document.getElementById("social-previous")
+
+
+const selfHours = document.getElementById("self-hours")
+const selfPrevious = document.getElementById("self-previous")
+ 
+
 // Daily Button
 
 document.getElementById("daily-btn").addEventListener("click", function(){
   fetch("./data.json")
   .then(response => response.json())
   .then(data => {
-  document.getElementById("work-hours").innerText = data[0].timeframes.daily
-  console.log()
+
+    //work
+  workHours.innerText =`${data[0].timeframes.daily.current} hrs `
+  workPrevious.innerText = `Last Week - ${data[0].timeframes.daily.previous} hrs `
+
+  // play
+  playhours.innerText = `${data[1].timeframes.daily.current} hrs`
+  playPrevious.innerText = ` Last Week - ${data[1].timeframes.daily.previous} hrs`
+
+  // study
+  studyHours.innerText = `${data[2].timeframes.daily.current} hrs`
+
   });
 })
 
