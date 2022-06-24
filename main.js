9baec1fe3d6427aee35f7f1642ca76b0a6098f9d
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-06-24 13:56:08 -0400

    s

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ document.getElementById("daily-btn").addEventListener("click", function(){
   .then(response => response.json())
   .then(data => {
 
-    //work
+  //work
   workHours.innerText =`${data[0].timeframes.daily.current} hrs `
   workPrevious.innerText = `Last Week - ${data[0].timeframes.daily.previous} hrs `
 
@@ -62,28 +62,79 @@ document.getElementById("daily-btn").addEventListener("click", function(){
 
 // Weekly Button
 
-document.getElementById("weekly-btn").addEventListener("click", daily)
-
-function daily(){
+document.getElementById("weekly-btn").addEventListener("click", function(){
   fetch("./data.json")
   .then(response => response.json())
   .then(data => {
   
-  });
+  //work
+  workHours.innerText =`${data[0].timeframes.weekly.current} hrs `
+  workPrevious.innerText = `Last Week - ${data[0].timeframes.weekly.previous} hrs `
 
-}
+  // play
+  playhours.innerText = `${data[1].timeframes.weekly.current} hrs`
+  playPrevious.innerText = ` Last Week - ${data[1].timeframes.weekly.previous} hrs`
+
+  // study
+  studyHours.innerText = `${data[2].timeframes.weekly.current} hrs`
+  studyPrevious.innerText = `Last Week ${data[2].timeframes.weekly.previous} hrs`
+
+  // Excercise
+  excerciseHours.innerText = `${data[3].timeframes.weekly.current} hrs`
+  excercisePrevious.innerText =`  Week ${data[3].timeframes.weekly.previous} hrs`
+
+
+  // Social
+  socialHours.innerText = `${data[4].timeframes.weekly.current} hrs`
+  socialPrevious.innerText =`Last Week ${data[3].timeframes.weekly.previous} hrs`
+
+  // Self Care
+  selfHours.innerText = `${data[4].timeframes.weekly.current} hrs`
+  selfPrevious.innerText =`Last Week ${data[3].timeframes.weekly.previous} hrs`
+  });
+})
+
+
+ 
 
 
 
 // Monthly Button
 
-document.getElementById("monthly-btn").addEventListener("click", daily)
-
-function daily(){
+document.getElementById("monthly-btn").addEventListener("click", function(){
   fetch("./data.json")
   .then(response => response.json())
   .then(data => {
-  
-  });
 
-}
+  //work
+  workHours.innerText =`${data[0].timeframes.monthly.current} hrs `
+  workPrevious.innerText = `Last Week - ${data[0].timeframes.monthly.previous} hrs `
+
+  // play
+  playhours.innerText = `${data[1].timeframes.monthly.current} hrs`
+  playPrevious.innerText = ` Last Week - ${data[1].timeframes.monthly.previous} hrs`
+
+  // study
+  studyHours.innerText = `${data[2].timeframes.monthly.current} hrs`
+  studyPrevious.innerText = `Last Week ${data[2].timeframes.monthly.previous} hrs`
+
+  // Excercise
+  excerciseHours.innerText = `${data[3].timeframes.monthly.current} hrs`
+  excercisePrevious.innerText =`  Week ${data[3].timeframes.weekly.previous} hrs`
+
+
+  // Social
+  socialHours.innerText = `${data[4].timeframes.monthly.current} hrs`
+  socialPrevious.innerText =`Last Week ${data[3].timeframes.monthly.previous} hrs`
+
+  // Self Care
+  selfHours.innerText = `${data[4].timeframes.weekly.current} hrs`
+  selfPrevious.innerText =`Last Week ${data[3].timeframes.monthly.previous} hrs`
+
+
+
+
+
+  });
+})
+
